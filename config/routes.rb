6fd7359a4048664ec62b1df4ac294cacd5403fd0
benja83 <tarennebenjamin@gmail.com>
2017-09-23 15:25:09 +0200

    Adds users api and list all users as json

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
     collection { post :import }
   end
 
+  namespace :api, defaults: { format: :json} do
+    namespace :v1 do
+      resources :users, only: [:index]
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
