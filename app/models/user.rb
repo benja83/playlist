060c9b737b4f79e7e1d8734dcf037e9c5e6e1293
benja83 <tarennebenjamin@gmail.com>
2017-09-24 13:36:16 +0200

    Adds playlist model, migration and association with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 class User < ActiveRecord::Base
   include Importable
 
+  has_many :playlists
+
   validates :email, uniqueness: true
   validates :user_name, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923142158) do
+ActiveRecord::Schema.define(version: 20170924113006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20170923142158) do
 
   add_index "mp3s", ["title"], name: "index_mp3s_on_title", using: :btree
 
+  create_table "playlists", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "playlists", ["user_id"], name: "index_playlists_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -41,4 +50,5 @@ ActiveRecord::Schema.define(version: 20170923142158) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["user_name"], name: "index_users_on_user_name", unique: true, using: :btree
 
+  add_foreign_key "playlists", "users"
 end
